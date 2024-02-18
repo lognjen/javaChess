178b4a3cdf570429d95593636cd06462f8005582
lognjen <ognjen.maksimovic@gmail.com>
2024-02-18 13:47:10 +0100

    Started work on Piece class

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -10,7 +10,7 @@ public abstract class Tile {
 
     protected final int tileCoordinate;
 
-    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+    private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
 
     private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
         final Map<Integer, EmptyTile> emptyTileMap =  new HashMap<>();
@@ -22,7 +22,7 @@ public abstract class Tile {
     }
 
     public static Tile createTile(final int tileCoordinate, final Piece piece){
-        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
     }
 
     private Tile(int tileCoordinate) {
@@ -34,7 +34,7 @@ public abstract class Tile {
     public abstract Piece getPiece();
 
     public static final class EmptyTile extends Tile{
-        EmptyTile(final int coordinate){
+        private EmptyTile(final int coordinate){
             super(coordinate);
         }
 
@@ -52,7 +52,7 @@ public abstract class Tile {
 
     public static final class OccupiedTile extends Tile{
         private final Piece pieceOnTile;
-        OccupiedTile(int coordinate, Piece piece){
+        private OccupiedTile(int coordinate, Piece piece){
             super(coordinate);
             this.pieceOnTile = piece;
         }
