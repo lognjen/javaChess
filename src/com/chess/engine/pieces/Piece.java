f78485c095b7389143b1d715c6855408ae5c7155
lognjen <ognjen.maksimovic@gmail.com>
2024-02-22 15:00:08 +0100

    Started work on Knight class

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -8,6 +8,11 @@ import java.util.List;
 
 public abstract class Piece {
     protected final int piecePosition;
+
+    public Alliance getPieceAlliance() {
+        return pieceAlliance;
+    }
+
     protected final Alliance pieceAlliance;
 
     Piece(final int piecePosition, final Alliance pieceAlliance){
